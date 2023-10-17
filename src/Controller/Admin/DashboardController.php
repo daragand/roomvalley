251058cd784134740d113f56ce5b2bd95bbcf0a7
OneMi785 <gorguluahmet78@gmail.com>
2023-10-17 17:56:35 +0200

    Ajout des liens de toutes pages d'admins via EasyAdmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Entity\Address;
+use App\Entity\Equipment;
+use App\Entity\Ergonomy;
+use App\Entity\ImagesRoom;
+use App\Entity\Reservation;
+use App\Entity\Room;
+use App\Entity\Software;
+use App\Entity\Status;
+use App\Entity\TypeEquipment;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+
+            ->setTitle('Administration')
+            ->renderContentMaximized();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard("Panneau d'administration", 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa-solid fa-users', User::class);
+        yield MenuItem::linkToCrud('Adresses', 'fa-solid fa-location-dot', Address::class);
+        yield MenuItem::linkToCrud('Équipements', 'fa-solid fa-toolbox', Equipment::class);
+        yield MenuItem::linkToCrud('Ergonomies', 'fa-solid fa-wheelchair', Ergonomy::class);
+        yield MenuItem::linkToCrud('Images room', 'fa-regular fa-image', ImagesRoom::class);
+        yield MenuItem::linkToCrud('Réservations', 'fa-solid fa-user-tie', Reservation::class);
+        yield MenuItem::linkToCrud('Salles', 'fa-solid fa-mug-saucer', Room::class);
+        yield MenuItem::linkToCrud('Logiciels', 'fa-solid fa-laptop-code', Software::class);
+        yield MenuItem::linkToCrud('Statuts', 'fa-solid fa-network-wired', Status::class);
+        yield MenuItem::linkToCrud("Type d'équipements", 'fa-solid fa-toolbox', TypeEquipment::class);
+    }
+}
