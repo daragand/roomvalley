1cbb66974bf2cb5f7917d9e529bf1ee3a4fa14a7
Demba Thiam <daragan.dooms@gmail.com>
2023-10-18 19:06:44 +0200

    ➕ ajout de UUId de Symfony pour Room et générer le slug 💥changement des fixtures

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: RoomRepository::class)]
 class Room
@@ -266,9 +267,9 @@ class Room
         return $this->slug;
     }
 
-    public function setSlug(string $slug): static
+    public function setSlug(): static
     {
-        $this->slug = $slug;
+        $this->slug = Uuid::v4()->__toString();
 
         return $this;
     }
