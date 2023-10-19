b385eb942cddcc3fbc37954096d91f4f45755b06
Demba Thiam <daragan.dooms@gmail.com>
2023-10-19 07:25:06 +0200

    ✨ création de la route app_room_show création de la vue sans les données depuis la vue room.

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Room;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_page')]
-    public function index(): Response
+    public function index(
+        Room $room,
+    ): Response
     {
         return $this->render('page/room.html.twig', [
             'controller_name' => 'PageController',
+            'room' => $room
+        ]);
+    }
+
+    #[Route('/room/{slug}', name: 'app_room_show')]
+    public function room(
+        Room $room,
+    ): Response
+    {
+        return $this->render('page/room_show.html.twig', [
+            'controller_name' => 'PageController',
+            'room' => $room
         ]);
     }
 }
