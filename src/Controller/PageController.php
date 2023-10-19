633e4a49f3a9aa2170af1d6b98f11a54aa3b0c05
OneMi785 <gorguluahmet78@gmail.com>
2023-10-19 15:56:59 +0200

    début de congif page de contact

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Room;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageController extends AbstractController
 {
@@ -28,6 +31,45 @@ class PageController extends AbstractController
         return $this->render('page/room_show.html.twig', [
             'controller_name' => 'PageController',
             'room' => $room
+        ]);
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(
+        Request $request,
+        MailerInterface $mailer
+    ): Response {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $subject = $form->get('subject')->getData();
+            $firstname = $form->get('firstname')->getData();
+            $lastname = $form->get('lastname')->getData();
+            $email = $form->get('email')->getData();
+            $phone = $form->get('phone')->getData();
+            $message = $form->get('message')->getData();
+
+            // On paramètre le mail
+            $mail = (new Email())
+                ->from($email)
+                ->to('contact@wecine.fr')
+                ->priority(Email::PRIORITY_HIGH)
+                ->subject($subject)
+                ->html(
+                    '<div> Client : ' . $firstname . ' ' . $lastname . '<br>
+                    Numéro de téléphone : ' . $phone . ' <br>
+                    Message :<br>' . $message . '</div>'
+                );
+
+            //On envoie le mail
+            $mailer->send($mail);
+
+            // On affiche un message de confirmation
+            $this->addFlash('success', 'Votre message a bien été envoyé !');
+        }
+        return $this->render('page/contact.html.twig', [
+            'title' => 'Nous contacter',
+            'description' => 'Formulaire de contact de la plateforme WeCiné',
+            'contact' => $form,
         ]);
     }
 }
